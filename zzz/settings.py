4729957ae525fcbd1db455720467f0b8733baa47
soma115 <soma115@o2.pl>
2016-10-31 15:40:16 +0100

    cleanup

--- zzz/settings.py
+++ zzz/settings.py
@@ -75,14 +75,21 @@ WSGI_APPLICATION = 'zzz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'mydb',
+#         'USER': 'postgres',
+#         'PASSWORD': 'Abcd123$',
+#         'HOST': '',
+#         'PORT': '',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'mydb',
-        'USER': 'postgres',
-        'PASSWORD': 'Abcd123$',
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
