5870d4901b690b8590daad0a5ae52b3f3a9f9e31
soma115 <soma115@o2.pl>
2016-11-22 18:24:32 +0100

    cleanup

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Website, WebsiteCategory, WebPage
 
-# Register your models here.
 admin.site.register(Website)
 admin.site.register(WebsiteCategory)
 admin.site.register(WebPage)
-
